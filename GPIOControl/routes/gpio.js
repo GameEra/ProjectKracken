991b14723934c2ff815d5f4b0c484de1cf23fead
mana <mana.guru@gmail.com>
2016-01-10 22:38:27 -0700

    Kracken first commit

---
+++ GPIOControl/routes/gpio.js
@@ -0,0 +1,25 @@
+﻿var express = require('express');
+var uwp = require("uwp");
+uwp.projectNamespace("Windows");
+var url = require('url');
+
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res) {
+	var url_parts = url.parse(req.url, true);
+	var query = url_parts.query;
+	var gpioController = Windows.Devices.Gpio.GpioController.getDefault();
+	var pin = gpioController.openPin(parseInt(query.gpiopin));
+	pin.setDriveMode(Windows.Devices.Gpio.GpioPinDriveMode.output)
+	var currentValue;
+	if (query.state == "on") 
+		currentValue = Windows.Devices.Gpio.GpioPinValue.high;
+	else
+		currentValue = Windows.Devices.Gpio.GpioPinValue.low;
+	pin.write(currentValue);
+	pin.close();
+
+	res.send('<h1>GPIO ' + query.pin +  ' set to ' + query.state + '<h1>');
+});
+module.exports = router;
---
+++ GPIOControl/routes/users.js
@@ -0,0 +1,9 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res) {
+    res.send('<h1>Surya is an Awesome person with great ideas, however he is lazy<h1>');
+});
+
+module.exports = router;
