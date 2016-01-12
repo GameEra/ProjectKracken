b2c5d8e9ea74038a3ba9cc2dc65632bbdbab9296
GameEra <surya.mana200@gmail.com>
2016-01-11 22:57:55 -0700

    Revert "added control center"
    This reverts commit 5051b441b162321b3e319a7cdeb418f9bcddc927.

--- GPIOControl/routes/gpio.js
+++ GPIOControl/routes/gpio.js
@@ -8,18 +8,28 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function (req, res) {
 	var url_parts = url.parse(req.url, true);
-	var query = url_parts.query;
-	var gpioController = Windows.Devices.Gpio.GpioController.getDefault();
-	var pin = gpioController.openPin(parseInt(query.gpiopin));
-	pin.setDriveMode(Windows.Devices.Gpio.GpioPinDriveMode.output)
-	var currentValue;
+    var query = url_parts.query;
+    
+    /* 5,6,21 */
+    
+
 	if (query.state == "on") 
 		currentValue = Windows.Devices.Gpio.GpioPinValue.high;
 	else
-		currentValue = Windows.Devices.Gpio.GpioPinValue.low;
-	pin.write(currentValue);
-	pin.close();
+        currentValue = Windows.Devices.Gpio.GpioPinValue.low;
+    
+    var globalvar = "global.pin" + query.gpiopin;     
+    var globalvarAssignment = globalvar + " = pin";
+    var pin = eval(globalvar);
 
-	res.send('<h1>GPIO ' + query.pin +  ' set to ' + query.state + '<h1>');
+    if (pin === undefined) {
+        var gpioController = Windows.Devices.Gpio.GpioController.getDefault();
+        pin = gpioController.openPin(parseInt(query.gpiopin));
+        eval(globalvarAssignment);
+    }
+    pin.write(currentValue);
+    pin.setDriveMode(Windows.Devices.Gpio.GpioPinDriveMode.output);
+
+	res.send('<h1>GPIO ' + query.gpiopin +  ' set to ' + query.state + '<h1>');
 });
 module.exports = router;
